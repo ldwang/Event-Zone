7f070ad7e15c0e38e5f6de1d3f1c88ebb5f705c2
Long Wang <ldwang@longs-mac.local>
2016-06-21 16:36:31 -0600

    Use containerView to host the static tableView for Event Detail

---
+++ Event Zone/EventDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  EventDetailViewController.swift
+//  Event Zone
+//
+//  Created by Long Wang on 2016-06-20.
+//  Copyright © 2016 Long Wang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+import CoreData
+
+class   EventDetailViewController: UIViewController {
+    
+//  
+//    func datePickerChanged() {
+//        location1StartsLabel.text = NSDateFormatter.localizedStringFromDate(location1StartsDatePicker.date, dateStyle: NSDateFormatterStyle.ShortStyle, timeStyle: NSDateFormatterStyle.ShortStyle)
+//        location1EndsLabel.text = NSDateFormatter.localizedStringFromDate(location1EndsDatePicker.date, dateStyle: .ShortStyle, timeStyle: .ShortStyle)
+//    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       // datePickerChanged()
+    }
+
+
+}
